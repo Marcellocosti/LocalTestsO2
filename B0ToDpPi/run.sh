0a538d3c612a77bc2dfcada808da897fb7f5c2f1
marcellocosti <marcellodicostanzo00@gmail.com>
2025-07-16 09:15:59 +0200

    Updates

---
+++ B0ToDpPi/run.sh
@@ -0,0 +1,41 @@
+OPTION="-b --configuration json://config.json" # --pipeline track-propagation:4"
+LOGFILE="log.txt"
+
+o2-analysis-pid-tof-full $OPTION |\
+o2-analysis-pid-tof-base $OPTION |\
+o2-analysis-hf-pid-creator $OPTION |\
+o2-analysis-pid-tpc $OPTION |\
+o2-analysis-mccollision-converter $OPTION |\
+o2-analysis-ft0-corrected-table $OPTION |\
+o2-analysis-track-propagation $OPTION |\
+o2-analysis-multcenttable $OPTION |\
+o2-analysis-trackselection $OPTION |\
+o2-analysis-hf-track-index-skim-creator $OPTION |\
+o2-analysis-hf-candidate-selector-dplus-to-pi-k-pi $OPTION |\
+o2-analysis-hf-candidate-creator-3prong $OPTION |\
+o2-analysis-hf-data-creator-charm-had-pi-reduced $OPTION |\
+o2-analysis-timestamp $OPTION |\
+o2-analysis-pid-tpc-base $OPTION |\
+o2-analysis-track-to-collision-associator $OPTION |\
+o2-analysis-tracks-extra-v002-converter $OPTION |\
+o2-analysis-event-selection $OPTION --aod-writer-json OutputDirector.json --aod-parent-access-level 1 > $LOGFILE 2>&1
+
+# report status
+rc=$?
+if [ $rc -eq 0 ]; then
+  echo "No problems!"
+else
+  echo "Error: Exit code $rc"
+  echo "Check the log file $LOGFILE"
+  exit $rc
+fi
+
+mv AnalysisResults.root AnalysisResults_workflow.root
+# mv AO2D.root AO2D_workflow.root
+# rm localhost*
+
+touch ao2ds_to_merge.txt
+echo AO2D.root > ao2ds_to_merge.txt
+o2-aod-merger --input ao2ds_to_merge.txt --max-size 10000000000000 --output Tree.root
+rm ao2ds_to_merge.txt
+rm localhost*
--- DPlusflow/run.sh
+++ DPlusflow/run.sh
@@ -10,16 +10,15 @@ o2-analysis-timestamp $OPTION |\
 o2-analysis-qvector-table $OPTION |\
 o2-analysis-hf-task-flow-charm-hadrons $OPTION |\
 o2-analysis-hf-track-index-skim-creator $OPTION |\
-o2-analysis-hf-candidate-selector-d0 $OPTION |\
+o2-analysis-multcenttable $OPTION |\
 o2-analysis-event-selection $OPTION |\
-o2-analysis-multiplicity-table $OPTION |\
 o2-analysis-track-to-collision-associator $OPTION |\
+o2-analysis-hf-candidate-selector-dplus-to-pi-k-pi $OPTION |\
 o2-analysis-pid-tpc-base $OPTION |\
 o2-analysis-track-propagation $OPTION |\
 o2-analysis-trackselection $OPTION |\
-o2-analysis-centrality-table $OPTION |\
 o2-analysis-tracks-extra-v002-converter $OPTION |\
-o2-analysis-hf-candidate-creator-2prong $OPTION --aod-file @input_data.txt > "$LOGFILE" 2>&1
+o2-analysis-hf-candidate-creator-3prong $OPTION --resources-monitoring 2 --aod-file @input_data.txt --aod-writer-json OutputDirector.json > "$LOGFILE" 2>&1
 
 # report status
 rc=$?
@@ -31,3 +30,9 @@ else
   echo "Check the log file $LOGFILE"
   exit $rc
 fi
+
+touch ao2ds_to_merge.txt
+echo AO2D.root > ao2ds_to_merge.txt
+o2-aod-merger --input ao2ds_to_merge.txt --max-size 10000000000000 --output Tree.root
+rm ao2ds_to_merge.txt
+rm localhost*
