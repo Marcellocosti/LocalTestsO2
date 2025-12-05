13123e7a4f937880a5177ef4384f4743914c1817
Marcello Di Costanzo <mdicosta@aliceml.cern.ch>
2025-12-05 09:36:35 +0100

    Derived data creator dplus

---
+++ TestFiles/DataOOPass2/command.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+alien.py cp /alice/cern.ch/user/a/alihyperloop/jobs/0321/hy_3212438/AOD/001/AO2D.root file:/home/mdicosta/LocalTestsO2/TestFiles/DataOOPass2/Train533309/0321/hy_3212438/0001/AO2D.root
+alien.py cp /alice/cern.ch/user/a/alihyperloop/jobs/0321/hy_3212438/AOD/002/AO2D.root file:/home/mdicosta/LocalTestsO2/TestFiles/DataOOPass2/Train533309/0321/hy_3212438/0002/AO2D.root
+alien.py cp /alice/cern.ch/user/a/alihyperloop/jobs/0321/hy_3212438/AOD/003/AO2D.root file:/home/mdicosta/LocalTestsO2/TestFiles/DataOOPass2/Train533309/0321/hy_3212438/0003/AO2D.root
+alien.py cp /alice/cern.ch/user/a/alihyperloop/jobs/0321/hy_3212438/AOD/004/AO2D.root file:/home/mdicosta/LocalTestsO2/TestFiles/DataOOPass2/Train533309/0321/hy_3212438/0004/AO2D.root
+alien.py cp /alice/cern.ch/user/a/alihyperloop/jobs/0321/hy_3212438/AOD/005/AO2D.root file:/home/mdicosta/LocalTestsO2/TestFiles/DataOOPass2/Train533309/0321/hy_3212438/0005/AO2D.root
+alien.py cp /alice/cern.ch/user/a/alihyperloop/jobs/0321/hy_3212438/AOD/006/AO2D.root file:/home/mdicosta/LocalTestsO2/TestFiles/DataOOPass2/Train533309/0321/hy_3212438/0006/AO2D.root
+alien.py cp /alice/cern.ch/user/a/alihyperloop/jobs/0321/hy_3212438/AOD/007/AO2D.root file:/home/mdicosta/LocalTestsO2/TestFiles/DataOOPass2/Train533309/0321/hy_3212438/0007/AO2D.root
+alien.py cp /alice/cern.ch/user/a/alihyperloop/jobs/0321/hy_3212438/AOD/008/AO2D.root file:/home/mdicosta/LocalTestsO2/TestFiles/DataOOPass2/Train533309/0321/hy_3212438/0008/AO2D.root
+alien.py cp /alice/cern.ch/user/a/alihyperloop/jobs/0321/hy_3212438/AOD/009/AO2D.root file:/home/mdicosta/LocalTestsO2/TestFiles/DataOOPass2/Train533309/0321/hy_3212438/0009/AO2D.root
---
+++ TestFiles/MCPPRef/Train527003/command.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+alien.py cp /alice/cern.ch/user/a/alihyperloop/jobs/0315/hy_3155424/0001/AO2D.root file:/home/mdicosta/LocalTestsO2/TestFiles/MCPPRef/Train527003/0315/hy_3155424/0001/AO2D.root
+alien.py cp /alice/cern.ch/user/a/alihyperloop/jobs/0315/hy_3155424/0002/AO2D.root file:/home/mdicosta/LocalTestsO2/TestFiles/MCPPRef/Train527003/0315/hy_3155424/0002/AO2D.root
+alien.py cp /alice/cern.ch/user/a/alihyperloop/jobs/0315/hy_3155424/0003/AO2D.root file:/home/mdicosta/LocalTestsO2/TestFiles/MCPPRef/Train527003/0315/hy_3155424/0003/AO2D.root
+alien.py cp /alice/cern.ch/user/a/alihyperloop/jobs/0315/hy_3155424/0004/AO2D.root file:/home/mdicosta/LocalTestsO2/TestFiles/MCPPRef/Train527003/0315/hy_3155424/0004/AO2D.root
+alien.py cp /alice/cern.ch/user/a/alihyperloop/jobs/0315/hy_3155424/0005/AO2D.root file:/home/mdicosta/LocalTestsO2/TestFiles/MCPPRef/Train527003/0315/hy_3155424/0005/AO2D.root
+alien.py cp /alice/cern.ch/user/a/alihyperloop/jobs/0315/hy_3155424/0006/AO2D.root file:/home/mdicosta/LocalTestsO2/TestFiles/MCPPRef/Train527003/0315/hy_3155424/0006/AO2D.root
+alien.py cp /alice/cern.ch/user/a/alihyperloop/jobs/0315/hy_3155424/0007/AO2D.root file:/home/mdicosta/LocalTestsO2/TestFiles/MCPPRef/Train527003/0315/hy_3155424/0007/AO2D.root
+alien.py cp /alice/cern.ch/user/a/alihyperloop/jobs/0315/hy_3155424/0008/AO2D.root file:/home/mdicosta/LocalTestsO2/TestFiles/MCPPRef/Train527003/0315/hy_3155424/0008/AO2D.root
+alien.py cp /alice/cern.ch/user/a/alihyperloop/jobs/0315/hy_3155424/0009/AO2D.root file:/home/mdicosta/LocalTestsO2/TestFiles/MCPPRef/Train527003/0315/hy_3155424/0009/AO2D.root
