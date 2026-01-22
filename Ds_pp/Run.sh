dd3423ab297741c0c2351dc562483742764f2041
fchinu <fabrizio.chinu@cern.ch>
2026-01-22 09:08:14 +0100

    Update Ds tests

--- Ds_pp/Run.sh
+++ Ds_pp/Run.sh
@@ -1,8 +1,17 @@
 LOGFILE="log.txt"
-OPTION="-b --configuration json://Config.json"
+OPTION="-b --configuration json://Config_derived.json"
 
-o2-analysis-hf-tree-creator-ds-to-k-k-pi $OPTION | o2-analysis-pid-tof-full $OPTION | o2-analysis-pid-tof-base $OPTION | o2-analysis-hf-pid-creator $OPTION | o2-analysis-pid-tpc $OPTION | o2-analysis-hf-task-ds $OPTION | o2-analysis-hf-candidate-selector-ds-to-k-k-pi $OPTION | o2-analysis-ft0-corrected-table $OPTION | o2-analysis-timestamp $OPTION | o2-analysis-pid-tpc-base $OPTION | o2-analysis-event-selection $OPTION | o2-analysis-track-propagation $OPTION | o2-analysis-multiplicity-table $OPTION | o2-analysis-mccollision-converter $OPTION | o2-analysis-multiplicity-table $OPTION | o2-analysis-hf-candidate-creator-3prong $OPTION | o2-analysis-trackselection $OPTION | o2-analysis-hf-track-index-skim-creator $OPTION | o2-analysis-pid-tpc-base $OPTION | o2-analysis-timestamp $OPTION | o2-analysis-centrality-table $OPTION | o2-analysis-ft0-corrected-table $OPTION | o2-analysis-event-selection $OPTION | o2-analysis-hf-pid-creator $OPTION | o2-analysis-track-propagation $OPTION | o2-analysis-trackselection $OPTION | o2-analysis-track-to-collision-associator $OPTION --aod-file @input_data.txt --aod-writer-json OutputDirector.json > "$LOGFILE" 2>&1
-
+o2-analysis-hf-task-ds $OPTION |
+o2-analysis-hf-candidate-selector-ds-to-k-k-pi $OPTION |
+o2-analysis-hf-candidate-creator-3prong $OPTION |
+o2-analysis-hf-pid-creator $OPTION |
+o2-analysis-pid-tof-merge $OPTION |
+o2-analysis-tracks-extra-v002-converter $OPTION |
+o2-analysis-event-selection-service $OPTION |
+o2-analysis-propagationservice $OPTION |
+o2-analysis-pid-tpc-service $OPTION |
+o2-analysis-mccollision-converter $OPTION |
+o2-analysis-ft0-corrected-table $OPTION --aod-file @input_data.txt --aod-writer-json OutputDirector.json --aod-parent-access-level 1 > $LOGFILE 2>&1
 # report status
 rc=$?
 if [ $rc -eq 0 ]; then
@@ -16,5 +25,5 @@ fi
 touch ao2ds_to_merge.txt
 echo AO2D.root > ao2ds_to_merge.txt
 o2-aod-merger --input ao2ds_to_merge.txt --max-size 10000000000000 --output Tree.root
-rm AO2D.root
+# rm AO2D.root
 rm ao2ds_to_merge.txt
