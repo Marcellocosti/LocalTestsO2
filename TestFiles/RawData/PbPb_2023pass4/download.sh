d992d4bbb0d93fd8b8b4034101209aef8eb613a4
Marcello Di Costanzo <mdicosta@aliceml.cern.ch>
2026-04-17 09:14:31 +0200

    Track Index PR

---
+++ TestFiles/RawData/PbPb_2023pass4/download.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+alien.py cp /alice/data/2023/LHC23zzh/544121/apass4/0240/o2_ctf_run00544121_orbit0076995424_tf0000000001_epn287/001/AO2D.root file:./AO2D_001.root
+alien.py cp /alice/data/2023/LHC23zzh/544121/apass4/0240/o2_ctf_run00544121_orbit0076995424_tf0000000001_epn287/002/AO2D.root file:./AO2D_002.root
+alien.py cp /alice/data/2023/LHC23zzh/544121/apass4/0240/o2_ctf_run00544121_orbit0076995424_tf0000000001_epn287/003/AO2D.root file:./AO2D_003.root
+
+# Can go up to 013
