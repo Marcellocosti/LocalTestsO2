2f6548d2db19c7d9e8ba9d784b1dd04b4d1fdbf2
marcellocosti <marcellodicostanzo00@gmail.com>
2026-05-07 12:13:46 +0200

    Updates for EsE

--- TestFiles/RawData/PbPb_2023pass4/download.sh
+++ TestFiles/RawData/PbPb_2023pass4/download.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-alien.py cp /alice/data/2023/LHC23zzh/544121/apass4/0240/o2_ctf_run00544121_orbit0076995424_tf0000000001_epn287/001/AO2D.root file:./AO2D_001.root
-alien.py cp /alice/data/2023/LHC23zzh/544121/apass4/0240/o2_ctf_run00544121_orbit0076995424_tf0000000001_epn287/002/AO2D.root file:./AO2D_002.root
-alien.py cp /alice/data/2023/LHC23zzh/544121/apass4/0240/o2_ctf_run00544121_orbit0076995424_tf0000000001_epn287/003/AO2D.root file:./AO2D_003.root
-
+alien.py cp /alice/data/2023/LHC23zzh/544124/apass4/0830/o2_ctf_run00544124_orbit0314201568_tf0000000001_epn290/001/AO2D.root file:./AO2D_PbPb_544124.root
+# alien.py cp /alice/data/2023/LHC23zzh/544121/apass4/0240/o2_ctf_run00544121_orbit0076995424_tf0000000001_epn287/001/AO2D.root file:./AO2D_001.root
+# alien.py cp /alice/data/2023/LHC23zzh/544121/apass4/0240/o2_ctf_run00544121_orbit0076995424_tf0000000001_epn287/002/AO2D.root file:./AO2D_002.root
+# alien.py cp /alice/data/2023/LHC23zzh/544121/apass4/0240/o2_ctf_run00544121_orbit0076995424_tf0000000001_epn287/003/AO2D.root file:./AO2D_003.root
 # Can go up to 013
+# alien.py cp /alice/data/2023/LHC23zzh/544091/apass4/1930/o2_ctf_run00544091_orbit0058228928_tf0000000001_epn315/001/AO2D.root file:./AO2D_004.root
