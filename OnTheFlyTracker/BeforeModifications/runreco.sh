9967802a71ed82efce47e2ffc90082ce605a13b0
marcellocosti <marcellodicostanzo00@gmail.com>
2026-05-08 11:41:22 +0200

    Update OTF PR

--- OnTheFlyTracker/BeforeModifications/runreco.sh
+++ OnTheFlyTracker/BeforeModifications/runreco.sh
@@ -6,13 +6,13 @@ export TBB_NUM_THREADS=1
 
 echo "Starting reconstruction in folder /home/mdicosta/LocalTestsO2/OnTheFlyTracker/BeforeModifications"
 
-o2-analysis-onthefly-tracker -b --configuration json://config_reco.json |
-o2-analysis-on-the-fly-detector-geometry-provider -b --configuration json://config_reco.json --aod-file AO2DGen.root --aod-writer-json OutputDir.json > log_reco.txt 2>&1
+o2-analysis-onthefly-tracker-old -b --configuration json://./../config_reco_old.json |
+o2-analysis-on-the-fly-detector-geometry-provider -b --configuration json://./../config_reco_old.json --aod-file @input.txt --aod-writer-json @../OutputDir.json > log_reco.txt 2>&1
 
 touch ao2ds_to_merge.txt
 echo AO2D.root > ao2ds_to_merge.txt
-o2-aod-merger --input ao2ds_to_merge.txt --max-size 1000000000000000000000000000 --output TreeReco20ps.root
+o2-aod-merger --input ao2ds_to_merge.txt --max-size 1000000000000000000000000000 --output TreeReco.root
 rm ao2ds_to_merge.txt
-mv AO2D.root AO2DReco20ps.root
-mv AnalysisResults.root AnalysisResultsReco20ps.root
-mv dpl-config.json dpl-config-reco-20-ps.json
+mv AO2D.root AO2DReco.root
+mv AnalysisResults.root AnalysisResultsReco.root
+mv dpl-config.json dpl-config-reco.json
