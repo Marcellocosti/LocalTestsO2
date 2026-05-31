94ed4449f1b2cf561cad0c227f58107b7fb1938b
marcellocosti <marcellodicostanzo00@gmail.com>
2026-05-31 10:59:52 +0200

    L1405

---
+++ Lambda1405/LikeSign/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+OPTION="-b --configuration json://config.json"
+LOGFILE="log.txt"
+
+o2-analysis-ft0-corrected-table $OPTION |\
+o2-analysis-pid-tof-merge $OPTION |\
+o2-analysis-multcenttable $OPTION |\
+o2-analysis-event-selection-service $OPTION |\
+o2-analysis-propagationservice $OPTION |\
+o2-analysis-pid-tpc-service $OPTION |\
+o2-analysis-lf-kink-builder $OPTION |\
+o2-analysis-tracks-extra-v002-converter $OPTION |\
+o2-analysis-lf-lambda1405analysis $OPTION --aod-writer-json OutputDir.json --aod-file @input_data.txt --shm-segment-size 7500000000 --aod-parent-access-level 1 severity debug > "$LOGFILE" 2>&1
+
+# report status
+rc=$?
+if [ $rc -eq 0 ]; then
+  echo "No problems!" >> "$LOGFILE"
+else
+  echo "Error: Exit code $rc" >> "$LOGFILE"
+  echo "Check the log file $LOGFILE"
+  exit $rc
+fi
+
+touch ao2ds_to_merge.txt
+echo AO2D.root > ao2ds_to_merge.txt
+o2-aod-merger --input ao2ds_to_merge.txt --max-size 10000000000000 --output Tree.root
+rm ao2ds_to_merge.txt
+rm localhost*
+rm AO2D.root
---
+++ Lambda1405/MC/inputs/LHC23k4i/download.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#!/bin/bash
+
+alien.py cp /alice/sim/2026/LHC23k4i/2/535069/AOD/001/AO2D.root file:./AO2D_001.root
+alien.py cp /alice/sim/2026/LHC23k4i/2/535069/AOD/002/AO2D.root file:./AO2D_002.root
+alien.py cp /alice/sim/2026/LHC23k4i/2/535069/AOD/003/AO2D.root file:./AO2D_003.root
+alien.py cp /alice/sim/2026/LHC23k4i/2/535069/AOD/004/AO2D.root file:./AO2D_004.root
+alien.py cp /alice/sim/2026/LHC23k4i/2/535069/AOD/005/AO2D.root file:./AO2D_005.root
+alien.py cp /alice/sim/2026/LHC23k4i/2/535069/AOD/006/AO2D.root file:./AO2D_006.root
+alien.py cp /alice/sim/2026/LHC23k4i/2/535069/AOD/007/AO2D.root file:./AO2D_007.root
+alien.py cp /alice/sim/2026/LHC23k4i/2/535069/AOD/008/AO2D.root file:./AO2D_008.root
+alien.py cp /alice/sim/2026/LHC23k4i/2/535069/AOD/009/AO2D.root file:./AO2D_009.root
+alien.py cp /alice/sim/2026/LHC23k4i/2/535069/AOD/010/AO2D.root file:./AO2D_010.root
+alien.py cp /alice/sim/2026/LHC23k4i/2/535069/AOD/011/AO2D.root file:./AO2D_011.root
